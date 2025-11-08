a5c13fcd972e8523d73a4e9b54be4bcff4c44344
Nova-Designs-Creative <novadesigns.creative@gmail.com>
2025-11-08 13:55:03 +0800

    done

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import { redirect } from "next/navigation";
 
 import { createClient } from "@/lib/supabase/server";
 import { InfoIcon } from "lucide-react";
@@ -7,11 +6,8 @@ import { FetchDataSteps } from "@/components/tutorial/fetch-data-steps";
 export default async function ProtectedPage() {
   const supabase = await createClient();
 
-  const { data, error } = await supabase.auth.getClaims();
-  if (error || !data?.claims) {
-    redirect("/auth/login");
-  }
-
+  const { data } = await supabase.auth.getClaims();
+  
   return (
     <div className="flex-1 w-full flex flex-col gap-12">
       <div className="w-full">
@@ -24,7 +20,7 @@ export default async function ProtectedPage() {
       <div className="flex flex-col gap-2 items-start">
         <h2 className="font-bold text-2xl mb-4">Your user details</h2>
         <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
-          {JSON.stringify(data.claims, null, 2)}
+          {JSON.stringify(data?.claims, null, 2)}
         </pre>
       </div>
       <div>
